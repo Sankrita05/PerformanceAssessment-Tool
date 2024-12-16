e429e072d273f24e4f8703564e12a42a88b36b84
Sankrita05 <sankritapatel0511@outlook.com>
2024-12-16 23:34:27 +0530

    Data Transformation Added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -23,7 +23,8 @@ class DataIngestion:
         # Read from the SQL Databases
         try:
             logging.info("Data Ingestion Started...")
-            df = read_sql_data()
+            # df = read_sql_data()
+            df = pd.read_csv(os.path.join('artifacts', 'raw.csv'))
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -3,12 +3,16 @@ import sys
 from src.logger import logging
 from src.exception import CustomException
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
 
 if __name__=="__main__":
     try:
         logging.info("Execution has started...")
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_path, test_path= data_ingestion.initiate_data_ingestion()
+
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transformation(train_path=train_path, test_path=test_path)
         logging.info("Execution has Completed...")
     except Exception as e:
         raise CustomException(e, sys)
